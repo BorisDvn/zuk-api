c26142e82bc2ad895dbc969443b3b1f549858473
BorisDvn <dovonon@th-brandenburg.de>
2022-06-06 13:33:18 +0200

    add springdoc-openapi to pom

--- src/main/java/com/thb/zukapi/controller/InformationController.java
+++ src/main/java/com/thb/zukapi/controller/InformationController.java
@@ -2,6 +2,7 @@ package com.thb.zukapi.controller;
 
 import com.thb.zukapi.models.Information;
 import com.thb.zukapi.services.InformationService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +17,7 @@ public class InformationController {
     @Autowired
     protected InformationService informationService;
 
-    @ApiOperation("Get All Information")
+    @Operation(summary ="Get All Information")
     @GetMapping("")
     public List<Information> getAllInformation(@RequestParam(defaultValue = "0") Integer pageNo,
                                                @RequestParam(defaultValue = "10") Integer pageSize,
@@ -24,28 +25,28 @@ public class InformationController {
         return informationService.getAll(pageNo, pageSize, sortBy);
     }
 
-    @ApiOperation("Get One Information")
+    /*@Operation(summary ="Get One Information")
     @GetMapping("/{id}")
     public Information getInformationById(@ApiParam(name = "InformationId", value = "ID of the Information_obj") @PathVariable UUID id) {
         return informationService.getAdmin(id);
     }
 
-    @ApiOperation("Add One Information")
+    @Operation(summary ="Add One Information")
     @PostMapping("")
     public Information addInformation(
             @ApiParam(name = "Information", value = "Information to add") @RequestBody Information information) {
         return informationService.addInformation(information);
     }
 
-    @ApiOperation("Update Information")
+    @Operation(summary ="Update Information")
     @PutMapping("")
     public Information updateInformation(@ApiParam(name = "Information", value = "Information to update") @RequestBody Information information) {
         return informationService.updateInformation(information);
     }
 
-    @ApiOperation("Delete Information")
+    @Operation(summary ="Delete Information")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteInformation(@ApiParam(name = "InformationId", value = "Id of the Information") @PathVariable UUID id) {
         return informationService.deleteInformation(id);
-    }
+    }*/
 }
--- src/main/java/com/thb/zukapi/models/Fluechtling.java
+++ src/main/java/com/thb/zukapi/models/Fluechtling.java
@@ -1,4 +1,23 @@
 package com.thb.zukapi.models;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Fluechtling {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    UUID id;
 }
