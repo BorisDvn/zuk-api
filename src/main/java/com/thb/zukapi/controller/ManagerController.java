edf6e816b5fe2b7703f8e3d285e7d34563ec5ca5
BorisDvn <64920674+BorisDvn@users.noreply.github.com>
2022-06-17 00:18:18 +0200

    implement Service and Controller for managerDTO (#17)

---
+++ src/main/java/com/thb/zukapi/controller/ManagerController.java
@@ -0,0 +1,72 @@
+package com.thb.zukapi.controller;
+
+import com.thb.zukapi.models.Manager;
+import com.thb.zukapi.models.Seeker;
+import com.thb.zukapi.services.ManagerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("zuk-api/v1/manager")
+@CrossOrigin(origins = "*")
+public class ManagerController {
+    @Autowired
+    protected ManagerService managerService;
+
+    @Operation(summary = "Get All Manager")
+    @ApiResponse(responseCode = "200", description = "Found all Manager",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Seeker.class))})
+    @GetMapping("")
+    public List<Manager> getAllManager(@RequestParam(defaultValue = "0") Integer pageNo,
+                                       @RequestParam(defaultValue = "10") Integer pageSize,
+                                       @RequestParam(defaultValue = "nachname") String sortBy) {
+        return managerService.getAll(pageNo, pageSize, sortBy);
+    }
+
+    @Operation(summary = "Get a Manager by its id")
+    @ApiResponse(responseCode = "200", description = "Found the Manager",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Manager.class))})
+    @GetMapping("/{id}")
+    public Manager getManagerById(@Parameter(name = "ManagerId", description = "ID of the Manager_obj") @PathVariable UUID id) {
+        return managerService.getManager(id);
+    }
+
+    @Operation(summary = "Add One Manager")
+    @ApiResponse(responseCode = "200", description = "Manager added",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Manager.class))})
+    @PostMapping("")
+    public Manager addManager(
+            @Parameter(name = "Manager", description = "Manager_obj to add") @RequestBody Manager manager) {
+        return managerService.addManager(manager);
+    }
+
+    @Operation(summary = "Update Manager")
+    @ApiResponse(responseCode = "200", description = "News Manager",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Manager.class))})
+    @PutMapping("")
+    public Manager updateManager(@Parameter(name = "Manager", description = "Manager_obj to update") @RequestBody Manager manager) {
+        return managerService.updateManager(manager);
+    }
+
+    @Operation(summary = "Delete a Manager by its id")
+    @ApiResponse(responseCode = "200", description = "News Manager",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Manager.class))})
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteManager(@Parameter(name = "ManagerId", description = "Id of the Manager to delete") @PathVariable UUID id) {
+        return managerService.deleteManagerById(id);
+    }
+}
--- src/main/java/com/thb/zukapi/services/ManagerService.java
+++ src/main/java/com/thb/zukapi/services/ManagerService.java
@@ -1,10 +1,95 @@
 package com.thb.zukapi.services;
 
+import com.thb.zukapi.exception.ApiRequestException;
+import com.thb.zukapi.models.Manager;
+import com.thb.zukapi.models.RoleType;
+import com.thb.zukapi.repositories.ManagerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ManagerService {
     private final Logger logger = LoggerFactory.getLogger(ManagerService.class);
+    @Autowired
+    private ManagerRepository managerRepository;
+
+    public Manager getManager(UUID id) {
+        return managerRepository.findById(id)
+                .orElseThrow(() -> new ApiRequestException("Cannot find Manager with id: " + id));
+    }
+
+    public List<Manager> getAll(Integer pageNo, Integer pageSize, String sortBy) {
+
+        Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+        Page<Manager> pagedResult = managerRepository.findAll(paging);
+
+        return pagedResult.getContent();
+    }
+
+    public Manager addManager(Manager manager) {
+
+        Manager newManager = new Manager();
+
+        newManager.setLastname(manager.getLastname());
+        newManager.setFirstname(manager.getFirstname());
+        if (manager.getNationality() != null) {
+            newManager.setNationality(manager.getNationality());
+        }
+        newManager.setDob(manager.getDob());
+        newManager.setPhone(manager.getPhone());
+        newManager.setEmail(manager.getEmail());
+        newManager.setAdresse(manager.getAdresse());
+        newManager.setGender(manager.getGender());
+        newManager.setPassword(manager.getPassword()); // TODO: encrypt
+        newManager.setRole(RoleType.MANAGER);
+
+        return managerRepository.save(newManager);
+    }
+
+    public Manager updateManager(Manager manager) {
+
+        Manager managerToUpdate = getManager(manager.getId());
+
+        if (manager.getLastname() != null)
+            managerToUpdate.setLastname(manager.getLastname());
+        if (manager.getFirstname() != null)
+            managerToUpdate.setFirstname(manager.getFirstname());
+        if (manager.getNationality() != null)
+            managerToUpdate.setNationality(manager.getNationality());
+        if (manager.getDob() != null) // TODO: check
+            managerToUpdate.setDob(manager.getDob());
+        if (manager.getPhone() != null)
+            managerToUpdate.setPhone(manager.getPhone());
+        if (manager.getEmail() != null)
+            managerToUpdate.setEmail(manager.getEmail());
+        if (manager.getAdresse() != null)
+            managerToUpdate.setAdresse(manager.getAdresse());
+        if (manager.getGender() != null)
+            managerToUpdate.setGender(manager.getGender());
+        if (manager.getPassword() != null)
+            managerToUpdate.setPassword(manager.getPassword()); // TODO: encrypt
+
+        return managerRepository.save(managerToUpdate);
+    }
+
+    public ResponseEntity<String> deleteManagerById(UUID id) {
+        Manager managerToDelete = getManager(id);
+
+        managerRepository.deleteById(managerToDelete.getId());
+
+        logger.info("Manager successfully deleted");
+        return new ResponseEntity<>("Manager successfully deleted", HttpStatus.OK);
+    }
+
 }
