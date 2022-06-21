05ab395d10f4aaeeadecd965b82562b8e0a8a5d2
Baudouin Keunne <keunne.baudoin@yahoo.fr>
2022-06-21 09:16:58 +0200

    some refract

--- src/test/java/com/thb/zukapi/seeker/SeekerIT.java
+++ src/test/java/com/thb/zukapi/seeker/SeekerIT.java
@@ -1,9 +1,10 @@
-package com.thb.zukapi.seekerIT;
+package com.thb.zukapi.seeker;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.junit.jupiter.api.AfterEach;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.thb.zukapi.ItBase;
+import com.thb.zukapi.dtos.person.PersonWriteTO;
 import com.thb.zukapi.models.Seeker;
 
 import io.restassured.http.ContentType;
@@ -18,6 +20,8 @@ import io.restassured.http.ContentType;
 public class SeekerIT extends ItBase {
 
     Seeker seeker, seeker1;
+    
+    PersonWriteTO signupSeeker;
 
     @BeforeEach
     public void setup() {
@@ -37,14 +41,16 @@ public class SeekerIT extends ItBase {
     }
 
 
-    @Test
+    @Test // TODO check why the test fail
     public void createSeeker() {
-        Seeker create = buildSeeker();
+    	signupSeeker = buildSignup();
+    	
+    	System.out.println(signupSeeker);
 
         UUID id = UUID.fromString(
                 given()
                         .contentType(ContentType.JSON)
-                        .body(create)
+                        .body(signupSeeker)
                         .log().body()
                         .post("zuk-api/v1/seeker")
                         .then()
@@ -54,8 +60,8 @@ public class SeekerIT extends ItBase {
 
         Seeker seeker = seekerRepository.findById(id).get();
 
-        assertThat(create.getLastname(), is(seeker.getLastname()));
-        assertThat(create.getEmail(), is(seeker.getEmail()));
+        assertThat(signupSeeker.getLastname(), is(seeker.getLastname()));
+        assertThat(signupSeeker.getEmail(), is(seeker.getEmail()));
     }
     
     @Test
@@ -75,5 +81,21 @@ public class SeekerIT extends ItBase {
 
         assertThat(seeker.getLastname(), is(seeker.getLastname()));
         assertThat(seeker.getEmail(), is(seeker.getEmail()));
+    }
+    
+    @Test
+    public void deleteSeeker() {
+
+        given()
+                .contentType(ContentType.JSON)
+                .log().body()
+                .delete("zuk-api/v1/seeker/"+seeker.getId())
+                .then()
+                .log().body()
+                .statusCode(200);
+
+        Optional<Seeker> seeker_ = seekerRepository.findById(seeker.getId());
+
+        assertThat(seeker_.isPresent(), is(false));
     }
 }
