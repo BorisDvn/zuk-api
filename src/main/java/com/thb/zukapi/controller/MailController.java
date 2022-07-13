6cd7df24fb097d3ca629e6192027015d2a12c114
BorisDvn <dovonon@th-brandenburg.de>
2022-07-13 07:31:32 +0200

    Create MailController.java

---
+++ src/main/java/com/thb/zukapi/controller/MailController.java
@@ -0,0 +1,36 @@
+package com.thb.zukapi.controller;
+
+import com.thb.zukapi.models.Email;
+import com.thb.zukapi.services.MailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/mail")
+@CrossOrigin(origins = "*")
+public class MailController {
+    private final MailService mailService;
+
+    public MailController(MailService mailService) {
+        this.mailService = mailService;
+    }
+
+    @Operation(summary = "Send simple email without attachment")
+    @PostMapping(value = "/simple-email")
+    public ResponseEntity<String> sendAttachmentEmail(
+            @Parameter(name = "mail", description = "mail to send") @RequestPart Email mail) {
+        return mailService.sendHtmlMessage(mail);
+    }
+
+    @Operation(summary = "Send email with attachment")
+    @PostMapping(value = "/attachment")
+    public ResponseEntity<String> sendAttachmentEmail(@Parameter(name = "mail", description = "Email to send") @RequestPart Email mail,
+                                                      @Parameter(name = "Files", description = "Liste of attachment") @RequestPart List<MultipartFile> files) {
+        return mailService.sendHtmlMessageAttachment(mail, files);
+    }
+}
